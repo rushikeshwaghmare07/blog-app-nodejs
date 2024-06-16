73ca0ebcb87e48d07f592ea054884c8f7b975b36
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-06-16 14:31:18 +0530

    add home route

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
 app.listen(PORT, () => console.log(`Server is started at PORT: ${PORT}`));
